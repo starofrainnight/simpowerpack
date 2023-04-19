044c040503b22ca0e6d136e0446312cd64e5fde4
Hong-She Liang <sorn@taorules.com>
2023-04-19 21:47:03 +0800

    Added method to split path elements

--- src/spppath.py
+++ src/spppath.py
@@ -15,3 +15,25 @@ def SppPathGetBaseName(path: str) -> str:
 
     baseNameIdx = sppstr.SppStrRFind2(path, "\\")
     return path[baseNameIdx + 1 : pathLen]
+
+
+def SppPathGetDirName(path: str) -> str:
+    pathLen = len(path)
+    if pathLen <= 0:
+        return ""
+
+    baseNameIdx = sppstr.SppStrRFind2(path, "\\")
+    return path[0:baseNameIdx]
+
+
+def SppPathGetExtName(path: str) -> str:
+    pathLen = len(path)
+    if pathLen <= 0:
+        return ""
+
+    baseName = SppPathGetBaseName(path)
+    dotIndex = sppstr.SppStrRFind2(baseName, ".")
+    if dotIndex < 0:
+        return ""
+
+    return baseName[dotIndex:pathLen]
